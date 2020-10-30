18c85fdc5cccfb3c4d842e74c087f5c3f025d4be
BosEriko <bos.eriko@gmail.com>
2020-10-30 13:11:23 +0800

    Hotfix: Remove unused export on native utilities

--- native/index.js
+++ native/index.js
@@ -1,6 +1,1 @@
 export { getCachedData, storeCache } from './caching'
-export { computeContentKey } from '../computeContentKey'
-export { sanitizeContent } from '../sanitizeContent'
-export { api } from '../api'
-
-export { renderChunk } from '../renderChunk.jsx'
