e2a018c5d8531b6ac96a8861bae0d2ba5c53c992
Jen <jengkarlong@gmail.com>
2020-12-02 17:07:20 +0800

    Revert "Allow object in chunk child, add custom chunk attribute"
    This reverts commit 7bf6a7ad903a24d739fc082d7944adda5f0c99a6.

--- computeContentKey.js
+++ computeContentKey.js
@@ -2,7 +2,12 @@ import hash from "@emotion/hash";
 import kebabCase from "lodash.kebabcase";
 
 export function computeContentKey(content) {
-  if (typeof content !== "string") return;
+  if (typeof content !== "string") {
+    console.error(
+      `Cannot compute chunk.content_key. Expected a string, received ${typeof content}.`
+    );
+    return;
+  }
 
   return `${kebabCase(content.slice(0, 32))}-${hash(content)}`;
 }
--- renderChunk.jsx
+++ renderChunk.jsx
@@ -2,15 +2,8 @@ import React from "react";
 import { sanitizeContent } from './'
 import ReactHtmlParser from 'react-html-parser';
 
-export const renderChunk = (chunk, Tag, props) => {
-  let parsedChunk;
-  if (chunk && typeof chunk.content == "string") {
-    const sanitizedData = sanitizeContent(chunk, props)
-    chunk = sanitizedData.chunk
-    parsedChunk = ReactHtmlParser(sanitizedData.parsedChunk)
-  } else {
-    parsedChunk = chunk.content;
-  }
+export const renderChunk = (data, Tag, props, textOnly = false) => {
+  const { chunk, parsedChunk } = sanitizeContent(data, props)
 
   const defaultprops = {
     "data-chunk": chunk.identifier,
@@ -20,8 +13,13 @@ export const renderChunk = (chunk, Tag, props) => {
     "key": chunk.identifier
   }
 
+  if (textOnly) {
+    return parsedChunk
+  }
+
   if (Tag == 'input') {
     return <input 
+      placeholder={parsedChunk}
       {...props} 
     />
   }
@@ -32,12 +30,12 @@ export const renderChunk = (chunk, Tag, props) => {
       return (<Tag
         {...defaultprops}
         {...props}
-      >{parsedChunk}</Tag>);
+      >{ReactHtmlParser(parsedChunk)}</Tag>);
     case "rich_text":
       return (<Tag
         {...defaultprops}
         {...props}
-      >{parsedChunk}</Tag>);
+      >{ReactHtmlParser(parsedChunk)}</Tag>);
     case "image":
       return (<img
         {...defaultprops}
